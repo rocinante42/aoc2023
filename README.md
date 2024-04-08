c160bc045b9f678ede82849680ae00e19ab16ea4
Roberto Amaya <rovertoo@gmail.com>
2024-04-08 08:33:30 -0600

    days 1 and 2 part 1

---
+++ README.md
@@ -0,0 +1,4 @@
+### Advent of code 2023
+
+Will update this file with the benchmarks of each solution
+
---
+++ day2/src/main.rs
@@ -0,0 +1,59 @@
+use color_eyre::{self, owo_colors::OwoColorize};
+use std::time::Instant;
+
+// my original solution but it happens to be a better one
+fn _main2() -> color_eyre::Result<()> {
+    // just for testing execution time
+    let start = Instant::now();
+
+    // start of program
+    let sum: u32 = include_str!("input.txt")
+        .lines()
+        .collect::<Vec<_>>()
+        .iter()
+        .map(|text_input| -> u32 {
+            // lets iterate on the str as bytes
+            // I could collect the number into a vector
+            let mut numbers: Vec<u32> = vec![];
+
+            for c in text_input.chars() {
+                match c.is_numeric() {
+                    true => numbers.push(c.to_digit(10).unwrap()),
+                    false => {}
+                }
+            }
+            format!("{}{}", numbers.first().unwrap(), numbers.last().unwrap())
+                .parse::<u32>()
+                .unwrap()
+        })
+        .sum();
+    // println!("sum is: {}", sum);
+
+    // end of program
+    let duration = start.elapsed();
+    println!("Time elapsed in main() is: {:?}", duration);
+    Ok(())
+}
+
+fn main() -> color_eyre::Result<()> {
+    // just for testing execution time
+    let start = Instant::now();
+
+    // start of program
+    //
+    let sum: u32 = include_str!("input.txt")
+        .lines()
+        .filter_map(|line| {
+            let mut nums = line.chars().filter_map(|ch| ch.to_digit(10));
+            let first = nums.next();
+            let last = nums.next_back().or(first);
+            Some((first.unwrap_or(0) * 10) + last.unwrap_or(0))
+        })
+        .sum();
+    println!("sum is: {}", sum);
+    // end of program
+    let duration = start.elapsed();
+    println!("Time elapsed in main() is: {:?}", duration);
+
+    Ok(())
+}
